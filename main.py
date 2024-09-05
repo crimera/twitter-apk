257171a3e9a2b4688eeabe332262b0fc7fa7b6f1
swakwork <swakwork+git@gmail.com>
2024-09-05 11:32:20 +0530

    Added manual build trigger

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from utils import panic, merge_apk, publish_release, report_to_telegram
 from download_bins import download_release_asset
 import apkmirror
 import os
+import argparse
 
 
 def get_latest_release(versions: list[Version]) -> Version | None:
@@ -14,36 +15,7 @@ def get_latest_release(versions: list[Version]) -> Version | None:
             return i
 
 
-def main():
-    # get latest version
-    url: str = "https://www.apkmirror.com/apk/x-corp/twitter/"
-    repo_url: str = "crimera/twitter-apk"
-
-    versions = apkmirror.get_versions(url)
-
-    latest_version = get_latest_release(versions)
-    if latest_version is None:
-        raise Exception("Could not find the latest version")
-
-    # only continue if it's a release
-    if latest_version.version.find("release") < 0:
-        panic("Latest version is not a release version")
-
-    last_build_version: github.GithubRelease | None = github.get_last_build_version(
-        repo_url
-    )
-
-    if last_build_version is None:
-        panic("Failed to fetch the latest build version")
-        return
-
-    # Begin stuff
-    if last_build_version.tag_name != latest_version.version:
-        print(f"New version found: {latest_version.version}")
-    else:
-        print("No new version found")
-        return
-
+def process(latest_version: Version):
     # get bundle and universal variant
     variants: list[Variant] = apkmirror.get_variants(latest_version)
 
@@ -106,5 +78,58 @@ Changelogs:
     report_to_telegram()
 
 
+def main():
+    # get latest version
+    url: str = "https://www.apkmirror.com/apk/x-corp/twitter/"
+    repo_url: str = "crimera/twitter-apk"
+
+    versions = apkmirror.get_versions(url)
+
+    latest_version = get_latest_release(versions)
+    if latest_version is None:
+        raise Exception("Could not find the latest version")
+
+    # only continue if it's a release
+    if latest_version.version.find("release") < 0:
+        panic("Latest version is not a release version")
+
+    last_build_version: github.GithubRelease | None = github.get_last_build_version(
+        repo_url
+    )
+
+    if last_build_version is None:
+        panic("Failed to fetch the latest build version")
+        return
+
+    # Begin stuff
+    if last_build_version.tag_name != latest_version.version:
+        print(f"New version found: {latest_version.version}")
+    else:
+        print("No new version found")
+        return
+    
+    process(latest_version)
+
+
+def manual(version:str):
+    link = f'https://www.apkmirror.com/apk/x-corp/twitter/twitter-{version.replace(".","-")}-release'
+    latest_version = Version(link=link,version=version)
+    process(latest_version)
+
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Piko APK')
+    # 0 = auto; 1 = manual;
+    parser.add_argument('--m', action="store", dest='mode', default=0)
+    parser.add_argument('--v', action="store", dest='version', default=0)
+    
+    args = parser.parse_args()
+    mode = args.mode
+    
+    if not mode: # auto
+        main()
+    else: # manual
+        version = args.version
+        if not version:
+            raise Exception("Version is required.")
+        manual(version)
