dbb8b5865bb3ecdfebf0187cfb4824fa9bd112ed
MondayNitro <87489540+MondayNitro@users.noreply.github.com>
2024-09-10 11:31:26 +0530

    allow fetching pre-releases

--- download_bins.py
+++ download_bins.py
@@ -3,16 +3,22 @@ import re
 from utils import download
 
 
-def download_release_asset(repo: str, regex: str, out_dir: str, filename=None):
-    url = f"https://api.github.com/repos/{repo}/releases/latest"
+def download_release_asset(repo: str, regex: str, out_dir: str, filename=None, include_prereleases: bool = True):
+    url = f"https://api.github.com/repos/{repo}/releases"
 
     response = requests.get(url)
     if response.status_code != 200:
         raise Exception("Failed to fetch github")
 
-    release = response.json()
+    # Use a list comprehension to filter based on pre-releases and regex
+    releases = [r for r in response.json() if (include_prereleases or r["tag_name"].find("release") >= 0)]
 
-    assets = response.json()["assets"]
+    if not releases:
+        raise Exception(f"No releases found for {repo}")  # Handle no releases case
+
+    latest_release = sorted(releases, key=lambda r: r["created_at"], reverse=True)[0]
+
+    assets = latest_release["assets"]
 
     link = None
     for i in assets:
@@ -22,9 +28,9 @@ def download_release_asset(repo: str, regex: str, out_dir: str, filename=None):
                 filename = i["name"]
             break
 
-    download(link, f"{out_dir.lstrip("/")}/{filename}")
+    download(link, f"{out_dir.lstrip('/')}/{filename}")
 
-    return release
+    return latest_release
 
 
 def download_apkeditor():
